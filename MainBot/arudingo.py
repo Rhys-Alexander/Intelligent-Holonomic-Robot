2b3498e3a4c4a35437bcd072b668a2b9679c3256
Gallahad072 <gallahad072@gmail.com>
2023-03-23 18:01:17 +0000

    test

--- MainBot/arudingo.py
+++ MainBot/arudingo.py
@@ -1,16 +1,22 @@
-import serial
+import serial, time
 
-ser = serial.Serial("dev/ttyUSB0", 9600)
+if __name__ == "__main__":
 
-while True:
-    data = input("Enter a word: ")
-    data = data.encode("utf-8")
-    ser.write(data)
-
-    if data == "!EXIT":
-        print("Disconneted from the server.")
-        break
-
-    data = ser.readline()
-    data = data.decode("utf-8")
-    print(f"Server: {data}")
+    print("Running. Press CTRL-C to exit.")
+    with serial.Serial("/dev/ttyUSB0", 9600, timeout=1) as arduino:
+        time.sleep(0.1)  # wait for serial to open
+        if arduino.isOpen():
+            print("{} connected!".format(arduino.port))
+            try:
+                while True:
+                    cmd = input("Enter command : ")
+                    arduino.write(cmd.encode())
+                    # time.sleep(0.1) #wait for arduino to answer
+                    while arduino.inWaiting() == 0:
+                        pass
+                    if arduino.inWaiting() > 0:
+                        answer = arduino.readline()
+                        print(answer)
+                        arduino.flushInput()  # remove data after reading
+            except KeyboardInterrupt:
+                print("KeyboardInterrupt has been caught.")
