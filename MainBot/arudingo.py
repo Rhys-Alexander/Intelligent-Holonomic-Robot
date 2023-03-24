292a732f22ef5d852fabfc4dd49677ace36cc82d
Gallahad072 <gallahad072@gmail.com>
2023-03-24 12:50:25 +0000

    fixed

--- MainBot/arudingo.py
+++ MainBot/arudingo.py
@@ -1,22 +1,22 @@
-import serial, time
+import serial
+import time
 
-if __name__ == "__main__":
 
-    print("Running. Press CTRL-C to exit.")
-    with serial.Serial("/dev/ttyUSB0", 9600, timeout=1) as arduino:
-        time.sleep(0.1)  # wait for serial to open
-        if arduino.isOpen():
-            print("{} connected!".format(arduino.port))
-            try:
-                while True:
-                    cmd = input("Enter command : ")
-                    arduino.write(cmd.encode())
-                    # time.sleep(0.1) #wait for arduino to answer
-                    while arduino.inWaiting() == 0:
-                        pass
-                    if arduino.inWaiting() > 0:
-                        answer = arduino.readline()
-                        print(answer)
-                        arduino.flushInput()  # remove data after reading
-            except KeyboardInterrupt:
-                print("KeyboardInterrupt has been caught.")
+print("Running. Press CTRL-C to exit.")
+with serial.Serial("/dev/ttyUSB0", 115200, timeout=1) as arduino:
+    time.sleep(0.1)  # wait for serial to open
+    if arduino.isOpen():
+        print("{} connected!".format(arduino.port))
+        try:
+            while True:
+                cmd = input("Enter command : ") + "\n"
+                arduino.write(cmd.encode())
+                while arduino.inWaiting() == 0:
+                    pass
+                while arduino.inWaiting() > 0:
+                    answer = arduino.readline()
+                    print(answer.decode())
+                    time.sleep(0.01)
+                arduino.flushInput()  # remove data after reading
+        except KeyboardInterrupt:
+            print("KeyboardInterrupt has been caught.")
