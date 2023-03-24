9f5518f884979d84cd235ec271e6684038fbda15
Gallahad072 <gallahad072@gmail.com>
2023-03-24 23:52:38 +0000

    newline

--- CTS/client.py
+++ CTS/client.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     while True:
-        data = input("Enter a word: ")
+        data = input("Enter a word: ") + "\n"
         data = data.encode()
         client.sendto(data, addr)
         if data == "!EXIT":
