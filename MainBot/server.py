885fef03da05ad419e2fb7081b88a237b4e95f59
Gallahad072 <gallahad072@gmail.com>
2023-03-26 22:45:07 +0100

    no longer sends data back and just receives

--- MainBot/server.py
+++ MainBot/server.py
@@ -13,20 +13,12 @@ with serial.Serial("/dev/ttyUSB0", 115200, timeout=1) as arduino:
     time.sleep(0.1)  # wait for serial to open
     if arduino.isOpen():
         print("{} connected!".format(arduino.port))
-        try:
-            while True:
-                data, addr = server.recvfrom(1024)
-                data = data.decode()
-                arduino.write(data.encode())
-                while arduino.inWaiting() == 0:
-                    pass
-                while arduino.inWaiting() > 0:
-                    answer = arduino.readline()
-                    print("arduino:", answer.decode())
-                    time.sleep(0.01)
-                data = "finished"
-                data = data.encode()
-                server.sendto(data, addr)
-                arduino.flushInput()  # remove data after reading
-        except KeyboardInterrupt:
-            print("KeyboardInterrupt has been caught.")
+        while True:
+            data, addr = server.recvfrom(1024)
+            data = data.decode()
+            arduino.write(data.encode())
+            while arduino.inWaiting() > 0:
+                answer = arduino.readline()
+                print("arduino:", answer.decode())
+                time.sleep(0.01)
+            arduino.flushInput()  # remove data after reading
