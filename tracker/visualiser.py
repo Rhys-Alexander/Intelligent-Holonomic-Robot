57f649e9c30d7f7571799c07059885cec5c74f8a
Gallahad072 <gallahad072@gmail.com>
2023-03-27 02:39:37 +0100

    generalised variables

--- tracker/visualiser.py
+++ tracker/visualiser.py
@@ -3,9 +3,6 @@ import math
 import cv2
 import numpy as np
 
-# y,x
-BOARD_DIMENSIONS = (2000, 2000)
-
 # Colours, BGR
 RED = (0, 0, 255)
 GREEN = (18, 170, 0)
@@ -13,7 +10,6 @@ BLUE = (230, 92, 0)
 YELLOW = (0, 191, 255)
 
 # Thicknesses
-BOARD_THICKNESS = 10
 ITEM_THICKNESS = 15
 BOT_THICKNESS = 20
 GRAPH_THICKNESS = 2
@@ -24,14 +20,11 @@ PUCK_RADIUS = 60
 BOT_RADIUS = 200
 
 
-class Board:
-    def __init__(self):
-        self.blank_state = 255 * np.ones(shape=[*BOARD_DIMENSIONS, 3], dtype=np.uint8)
-
+class View:
     def drawPuck(self, pt):
         x, y = pt
         cv2.circle(
-            self.board,
+            self.view,
             center=(x, y),
             radius=PUCK_RADIUS,
             color=YELLOW,
@@ -45,7 +38,7 @@ class Board:
             x, y = bot
             rot = None
         cv2.circle(
-            self.board,
+            self.view,
             center=(x, y),
             radius=radius,
             color=colour,
@@ -53,7 +46,7 @@ class Board:
         )
         if rot:
             cv2.line(
-                self.board,
+                self.view,
                 pt1=(x, y),
                 pt2=(
                     math.ceil(x + radius * np.cos(rot)),
@@ -63,7 +56,7 @@ class Board:
                 thickness=BOT_THICKNESS,
             )
             cv2.putText(
-                self.board,
+                self.view,
                 str(round(math.degrees(rot), 2)),
                 (x, y),
                 cv2.FONT_HERSHEY_SIMPLEX,
@@ -76,7 +69,7 @@ class Board:
         for i, x in enumerate(graph):
             if x:
                 cv2.line(
-                    self.board,
+                    self.view,
                     pt1=nodes[i],
                     pt2=bot[:2],
                     color=BLUE,
@@ -85,7 +78,7 @@ class Board:
 
     def drawPath(self, path, bot):
         cv2.line(
-            self.board,
+            self.view,
             pt1=path,
             pt2=bot[:2],
             color=GREEN,
@@ -103,7 +96,7 @@ class Board:
         img,
         cmd,
     ):
-        self.board = img
+        self.view = img
         if pucks:
             for puck in pucks:
                 self.drawPuck(puck)
@@ -117,7 +110,7 @@ class Board:
                 self.drawPath(path, bot)
         if cmd:
             cv2.putText(
-                self.board,
+                self.view,
                 cmd.strip(),
                 (10, 90),
                 cv2.FONT_HERSHEY_SIMPLEX,
@@ -125,4 +118,4 @@ class Board:
                 RED,
                 10,
             )
-        return self.board
+        return self.view
