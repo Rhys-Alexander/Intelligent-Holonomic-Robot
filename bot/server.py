c5df8eda5bdd1cf33dd09101754a25ab6673b7cc
Gallahad072 <gallahad072@gmail.com>
2023-03-27 16:14:33 +0100

    optimised for speed

--- bot/server.py
+++ bot/server.py
@@ -9,16 +9,14 @@ server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind((host, port))
 
 print("Running. Press CTRL-C to exit.")
-with serial.Serial("/dev/ttyUSB0", 115200, timeout=1) as arduino:
+with serial.Serial("/dev/ttyUSB0", 500000, timeout=1) as arduino:
     time.sleep(0.1)  # wait for serial to open
     if arduino.isOpen():
         print("{} connected!".format(arduino.port))
         while True:
             data, addr = server.recvfrom(1024)
-            data = data.decode()
-            arduino.write(data.encode())
+            arduino.write(data)
             while arduino.inWaiting() > 0:
                 answer = arduino.readline()
-                print("arduino:", answer.decode())
                 time.sleep(0.01)
             arduino.flushInput()  # remove data after reading
